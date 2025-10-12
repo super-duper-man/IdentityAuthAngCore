c675fdea7d84bacc026c070c66dd8f7628cf0814
Danny <m.r.fouladi7@gmail.com>
2025-10-12 13:44:00 +0330

    user registration validation

--- src/app/user/registration/registration.component.ts
+++ src/app/user/registration/registration.component.ts
@@ -1,26 +1,65 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import {
+  AbstractControl,
+  FormBuilder,
+  ReactiveFormsModule,
+  ValidatorFn,
+  Validators,
+} from '@angular/forms';
 import { FormGroup } from '@angular/forms';
+import { FirstKeyPipe } from '../../shared/pipes/first-key.pipe';
 
 @Component({
   selector: 'app-registration',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, CommonModule, FirstKeyPipe],
   templateUrl: './registration.component.html',
-  styleUrl: './registration.component.scss'
+  styleUrl: './registration.component.scss',
 })
-
 export class RegistrationComponent {
   form: FormGroup;
-  constructor(public formBuilder: FormBuilder){
-    this.form = this.formBuilder.group({
-      fullName: [''],
-      email: [''],
-      password: [''],
-      confirmPassword: [''],
-    });
+  isSubmitted: boolean = false;
+
+  constructor(public formBuilder: FormBuilder) {
+    this.form = this.formBuilder.group(
+      {
+        fullName: ['', Validators.required],
+        email: ['', [Validators.required, Validators.email]],
+        password: ['', [Validators.required, Validators.minLength(6)]],
+        confirmPassword: [''],
+      },
+      { validators: this.passwordMatchValidator }
+    );
   }
 
-  onSubmit(){
+  passwordMatchValidator: ValidatorFn = (control: AbstractControl): null => {
+    const password = control.get('password');
+    const confirmPassword = control.get('confirmPassword');
+
+    if (
+      password &&
+      confirmPassword &&
+      password.value !== confirmPassword.value
+    ) {
+      confirmPassword.setErrors({ passwordMismatch: true });
+    } else {
+      confirmPassword?.setErrors(null);
+    }
+
+    return null;
+  };
+
+  onSubmit() {
+    this.isSubmitted = true;
     console.log(this.form.value);
+  }
+
+  hasDisplayError(controlName: string): Boolean {
+    const control = this.form.get(controlName);
+
+    return (
+      Boolean(control?.invalid) &&
+      (this.isSubmitted || Boolean(control?.touched))
+    );
   }
 }
