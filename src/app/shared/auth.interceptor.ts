b4a4d5abf9234ff2cd4914431700a4a593c2ee29
Danny <m.r.fouladi7@gmail.com>
2025-10-15 10:45:04 +0330

    handle 401 and 403 errors

--- src/app/shared/auth.interceptor.ts
+++ src/app/shared/auth.interceptor.ts
@@ -1,9 +1,15 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { AuthService } from './services/auth.service';
+import { tap } from 'rxjs';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   const authService = inject(AuthService);
+  const router = inject(Router);
+  const toast = inject(ToastrService);
+
   if (authService.isLoggedIn()) {
     const cloneReq = req.clone({
       headers: req.headers.set(
@@ -12,7 +18,24 @@ export const authInterceptor: HttpInterceptorFn = (req, next) => {
       ),
     });
 
-    return next(cloneReq);
+    return next(cloneReq).pipe(
+      tap({
+        error: (err: any) => {
+          console.log(err);
+          if (err.status === 401) {
+            authService.deleteToken();
+            setTimeout(() => {
+              toast.info(
+                'لطفا دوباره وارد شوید، زمان مجوز شما به پایان رسیده است.'
+              );
+            }, 1500);
+            router.navigateByUrl('/signin');
+          } else if (err.status === 403) {
+            toast.error('کاربر شما مجوز لازم را ندارد.');
+          }
+        },
+      })
+    );
   }
   return next(req);
 };
